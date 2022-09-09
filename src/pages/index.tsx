f654168a493cdedc110274e08695449b782ab9da
ArminSH_32 <shapoori@appcosolution.com>
2022-09-09 16:33:38 +0430

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,9 @@
 import styles from '../styles/Home.module.css'
-import {Button, Div, TextField} from '@elements'
+import {Div} from '@elements'
 
 const Home = () => {
   return (
     <Div className={styles.container}>
-      <Button>
-        button
-      </Button>
-      <TextField
-        InputProps={{className: styles.input}}
-        className={styles.textField}
-        placeholder={"- - - - - - (98+)"}
-        placeholderAlign={"center"}
-        label={'شماره همراه خود را وارد کنید'}
-      />
     </Div>
   )
 }
