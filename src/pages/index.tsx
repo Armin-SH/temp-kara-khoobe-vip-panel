fdf26d16d3755be8617d8a04e2615387ca3698fd
ArminSH_32 <shapoori@appcosolution.com>
2022-09-10 22:35:04 +0430

    edit home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 import styles from '../styles/Home.module.css'
-import {Div} from '@elements'
+import {Button, Div, Text} from '@elements'
+import routes from '@routes'
 
 const Home = () => {
   return (
     <Div className={styles.container}>
+      <Text color={"grey.900"} type={"bold"} typography={"huge"}>
+        صفحه اصلی پنل
+      </Text>
+      <Button className={styles.button} href={routes['route.auth.login']}>
+        بازگشت
+      </Button>
     </Div>
   )
 }
