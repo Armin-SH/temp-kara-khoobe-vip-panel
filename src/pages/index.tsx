cdb65c3272da2700abd91233becb087fa28c45a1
ArminSH_32 <shapoori@appcosolution.com>
2022-10-03 10:04:41 +0330

    finish menu logic and mobile menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,38 @@
-import styles from '../styles/Home.module.css'
+import styles from '@styles/home/home.module.css'
 import {Div} from '@elements'
 import {RequestItem} from '@screens/home'
 
 const Home = () => {
   return (
-      <Div mobile={"column"} desktop={"row-reverse"} className={styles.container}>
-        <RequestItem
-            doneCounter={11}
-            inProgressCounter={26}
-            subTitle={'انجام شده'}
-            title={'در حال انجام'}
-            totalCounter={45}
-            buttonTitle={'مشاهده درخواست های جاری'}
-            header={'درخواست های جاری'}
-        />
-        <RequestItem
-            doneCounter={32}
-            inProgressCounter={85}
-            subTitle={'پرداخت نشده'}
-            title={'پرداخت شده'}
-            totalCounter={32}
-            buttonTitle={'مشاهده درخواست های گذشته'}
-            header={'درخواست های گذشته'}
-        />
-        <RequestItem
-            doneCounter={11}
-            inProgressCounter={26}
-            subTitle={'پرداخت نشده'}
-            title={'پرداخت شده'}
-            totalCounter={87}
-            buttonTitle={'مشاهده کل درخواست ها'}
-            header={'کل درخواست ها'}
-        />
-      </Div>
+    <Div mobile={"column"} desktop={"row-reverse"} className={styles.container}>
+      <RequestItem
+        doneCounter={11}
+        inProgressCounter={26}
+        subTitle={'انجام شده'}
+        title={'در حال انجام'}
+        totalCounter={45}
+        buttonTitle={'مشاهده درخواست های جاری'}
+        header={'درخواست های جاری'}
+      />
+      <RequestItem
+        doneCounter={32}
+        inProgressCounter={85}
+        subTitle={'پرداخت نشده'}
+        title={'پرداخت شده'}
+        totalCounter={32}
+        buttonTitle={'مشاهده درخواست های گذشته'}
+        header={'درخواست های گذشته'}
+      />
+      <RequestItem
+        doneCounter={11}
+        inProgressCounter={26}
+        subTitle={'پرداخت نشده'}
+        title={'پرداخت شده'}
+        totalCounter={87}
+        buttonTitle={'مشاهده کل درخواست ها'}
+        header={'کل درخواست ها'}
+      />
+    </Div>
   )
 }
 
--- src/pages/reports/index.tsx
+++ src/pages/reports/index.tsx
@@ -1,13 +1,58 @@
 import React from 'react'
 import {Div} from '@elements'
+import styles from '@styles/home/reports.module.css'
+import {Cell, Pie, PieChart} from 'recharts';
+
+const data = [
+  {name: 'Group A', value: 200},
+  {name: 'Group B', value: 200},
+  {name: 'Group C', value: 200},
+  {name: 'Group D', value: 200},
+  {name: 'Group E', value: 200},
+  {name: 'Group F', value: 200},
+  {name: 'Group G', value: 200},
+  {name: 'Group H', value: 200},
+  {name: 'Group I', value: 200},
+  {name: 'Group J', value: 200},
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const Reports = () => {
 
 
-const Profile = () => {
   return (
-    <Div>
+    <Div mobile={"column"}>
+      <Div className={styles.topContainer} desktop={"row-reverse"} tablet={"column"} mobile={"column"}>
+        <Div className={styles.performanceContainer}>
+          <PieChart width={318} height={368}>
+            <Pie
+              data={data}
+              cx={159}
+              width={250}
+              height={250}
+              cy={184}
+              startAngle={225}
+              endAngle={-45}
+              innerRadius={65}
+              outerRadius={80}
+              fill="#8884d8"
+              paddingAngle={4}
+              dataKey="value"
+            >
+              {data.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]}/>
+              ))}
+            </Pie>
+          </PieChart>
+        </Div>
+        <Div className={styles.chartContainer}>
 
+        </Div>
+      </Div>
+      <Div>
+      </Div>
     </Div>
   )
 }
 
-export default Profile
+export default Reports
